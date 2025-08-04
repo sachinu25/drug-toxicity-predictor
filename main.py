92c8defa74cdc2e232ad721a7e01358d966986c3
SACHIN UPADHYAY <sachinu037@gmail.com>
2025-08-04 19:01:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -422,7 +422,7 @@ def main():
         
         This model is for research and educational purposes only.
         CREATED BY SACHIN UPADHYAY
-        GITHUB LINK - https://github.com/sachinu25
+        GITHUB LINK - https://github.com/sachinu25/drug-toxicity-predictor
         """)
 
 if __name__ == "__main__":
